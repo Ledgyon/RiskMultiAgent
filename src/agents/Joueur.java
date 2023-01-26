042b4981433691daab9cd18a586fa61d5e0cb9a1
Simon Rudent <Ledgyon>
2023-01-26 17:55:18 +0100

    param Joueur +

--- src/agents/Joueur.java
+++ src/agents/Joueur.java
@@ -1,9 +1,18 @@
 package agents;
 
+import java.util.List;
+
+import carte.CarteMission;
+import carte.CartePioche;
+import plateau.Territoire;
+
 public class Joueur {
 
     private String couleur;
     private int nombreRegiment;
+    private List<CartePioche> main; // cartes que le joueur possede dans sa main (max 5)
+    private CarteMission objectif; // objectif du joueur pour remporter la partie
+    private List<Territoire> territoires; // territoires possedes par le joueur
 
     public boolean isDead(){
         return nombreRegiment == 0;
