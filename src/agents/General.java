1e4b99ef352b1a099460f490359ec5ae153b0ed8
Simon Rudent <Ledgyon>
2023-01-26 16:54:47 +0100

    pioche

--- src/agents/General.java
+++ src/agents/General.java
@@ -1,18 +1,30 @@
 package agents;
 
+import plateau.enumerations.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import carte.CarteMission;
 import carte.CartePioche;
+import carte.enumerations.Unite;
 
 public class General {
 	private List<CartePioche> pioche;
 	private List<CarteMission> objectifs;
 	
-	public General(List<CartePioche> pioche, List<CarteMission> objectifs) {
-		super();
-		this.pioche = pioche;
-		this.objectifs = objectifs;
+	
+	public General() {
+		this.pioche = new ArrayList<>();
+	
+		this.objectifs = new ArrayList<>();
+	}
+	
+	public void ConstructPioche()
+	{
+		CartePioche carte1 = new CartePioche(NomTerritoireASIE.AFGHANISTAN.toString(),Unite.FANTASSIN.toString());
+		this.pioche.add(carte1);
+		CartePioche carte2 = new CartePioche(NomTerritoireASIE.INDE.toString(),Unite.FANTASSIN.toString());
+		this.pioche.add(carte2);
 	}
 
 	public List<CartePioche> getPioche() {
